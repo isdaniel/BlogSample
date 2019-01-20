0dee651bca57f5a7beeb1ac97112c107efd6e415
Daniel <dog830228@gmail.com>
2019-01-20 23:11:29 +0800

    add rabbitmq sample

---
+++ src/Samples/RabbitMqClient/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMqConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory
+            {
+                UserName = "guest",
+                Password = "guest",
+                HostName = "localhost"
+            };
+
+            string exchangeName = "exchange2";
+            string routeKey = "hello";
+            string queueName = "q1";
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                //channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+                channel.ExchangeBind(exchangeName, exchangeName, routeKey);
+                //channel.QueueDeclare(queueName, false, false, false, null);
+                channel.QueueBind(queueName, exchangeName, routeKey); //綁定一個消費者
+       
+
+                EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+       
+                channel.BasicQos(0, 1, false);
+               
+                //接收到消息事件 consumer.IsRunning
+                consumer.Received += (ch, ea) =>
+                {
+                    var message = Encoding.UTF8.GetString(ea.Body);
+
+                    Console.WriteLine($"Queue:{queueName}收到資料： {message}");
+                    channel.BasicAck(ea.DeliveryTag, false);
+                };
+
+                channel.BasicConsume(queueName, false, consumer);
+
+              
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ src/Samples/RabbitMqProducer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace RabbitMqProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //建立連接工廠
+            ConnectionFactory factory = new ConnectionFactory
+            {
+                UserName = "guest",
+                Password = "guest",
+                HostName = "localhost"
+            };
+            
+            string exchangeName = "exchange2";
+            string routeKey = "hello";
+            string queueName = "q1";
+
+            using (var connection = factory.CreateConnection())//创建通道
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queueName, false, false, false, null);
+                channel.ExchangeDeclare(exchangeName, ExchangeType.Fanout, false, false, null);
+
+                channel.QueueBind(queueName,exchangeName,routeKey);
+       
+
+                Console.WriteLine("\nRabbitMQ連接成功,如需離開請按下Escape鍵");
+
+                string input = string.Empty;
+                do
+                {
+                    input = Console.ReadLine();
+
+                    var sendBytes = Encoding.UTF8.GetBytes(input);
+                    //發布訊息到RabbitMQ Server
+                    channel.BasicPublish(exchangeName, routeKey, null, sendBytes);
+
+                } while (Console.ReadKey().Key != ConsoleKey.Escape);
+            }
+        }
+    }
+}
