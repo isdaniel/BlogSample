9442ccd301df7101187506548850f49282fb6945
Daniel <dog830228@gmaill.com>
2021-04-08 17:13:15 +0800

    add src\Samples\ExpressionVsReflection

---
+++ src/Samples/ExpressionVsReflection/ObjectProvider.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace ExpressionVsReflection
+{
+    public delegate T Func<T>(params object[] args);
+    public class ObjectProvider
+    {
+
+        private static ConcurrentDictionary<string,Delegate> _mapFunc = new ConcurrentDictionary<string, Delegate>();
+
+        public static T ReflectionCreator<T>(params object[] args)
+            where T : class
+        {
+            return Activator.CreateInstance(typeof(T), args) as T;
+        }
+
+        public static Func<T> ExpressionCreator<T>()
+        {
+            var key = typeof(T).Name;
+
+            if (!_mapFunc.TryGetValue(key, out Delegate result))
+            {
+                ConstructorInfo ctor = typeof(T).GetConstructors().FirstOrDefault();
+
+                ParameterInfo[] paramsInfo = ctor.GetParameters();
+
+                ParameterExpression param =
+                    Expression.Parameter(typeof(object[]), "args");
+
+                Expression[] argsExp =
+                    new Expression[paramsInfo.Length];
+
+                for (int i = 0; i < paramsInfo.Length; i++)
+                {
+                    Expression index = Expression.Constant(i);
+                    Type paramType = paramsInfo[i].ParameterType;
+
+                    Expression paramAccessorExp =
+                        Expression.ArrayIndex(param, index);
+
+                    Expression paramCastExp =
+                        Expression.Convert(paramAccessorExp, paramType);
+
+                    argsExp[i] = paramCastExp;
+                }
+
+                NewExpression newExp = Expression.New(ctor, argsExp);
+
+                LambdaExpression lambda = Expression.Lambda(typeof(Func<T>), newExp, param);
+
+                result = lambda.Compile();
+
+                _mapFunc.GetOrAdd(key, result);
+            }
+
+
+            return (Func<T>)result;
+        }
+    }
+}
---
+++ src/Samples/ExpressionVsReflection/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Order;
+using BenchmarkDotNet.Running;
+
+namespace ExpressionVsReflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+   
+
+            var b = BenchmarkRunner.Run<BenchmarkMeasure>(new Config());
+            Console.Read();
+        }
+    }
+
+    [BenchmarkCategory("Framework")]
+    [MemoryDiagnoser]
+    [SimpleJob(launchCount: 1, warmupCount: 1, targetCount: 1, invocationCount: 1, baseline: false)]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    public class BenchmarkMeasure
+    {
+        [Benchmark(Description = "ReflectionCreator no parameter")]
+        public void ReflectionCreatorNoParameter()
+        {
+            ObjectProvider.ReflectionCreator<A>();
+        }
+
+        [Benchmark(Description = "ReflectionCreator had parameter")]
+        public void ReflectionCreatorParameter()
+        {
+            ObjectProvider.ReflectionCreator<ParaA>(1);
+        }
+
+        [Benchmark(Description = "ExpressionCreator no parameter")]
+        public void ExpressionCreatorNoParameter()
+        {
+            ObjectProvider.ExpressionCreator<A>();
+        }
+
+        [Benchmark(Description = "ExpressionCreator had parameter")]
+        public void ExpressionCreatorParameter()
+        {
+            ObjectProvider.ExpressionCreator<ParaA>()(1);
+        }
+    }
+
+    public class A
+    {
+    }
+
+    public class ParaA
+    {
+        private readonly int _val;
+
+        public ParaA(int val)
+        {
+            _val = val;
+        }
+    }
+}
