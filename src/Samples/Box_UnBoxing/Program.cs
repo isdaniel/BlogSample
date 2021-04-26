e7c231382bdc88077eb93c941661f71aa6569e29
Daniel <dog830228@gmaill.com>
2021-04-26 18:32:29 +0800

    use BenchmarkDotNet src\Samples\Box_UnBoxing\

--- src/Samples/Box_UnBoxing/Program.cs
+++ src/Samples/Box_UnBoxing/Program.cs
@@ -4,6 +4,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using BenchmarkDotNet.Running;
 
 namespace Box_UnBoxing
 {
@@ -11,27 +14,48 @@ namespace Box_UnBoxing
     {
         static void Main(string[] args)
         {
-            int times = 30000000;
-            string s = string.Empty;
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            for (int i = 0; i < times ; i++)
-            {
-                s = $"{times}";
-            }
-            
-            sw.Stop();
-            Console.WriteLine($"times:{sw.ElapsedMilliseconds}");
-
-            sw.Restart();
-            for (int i = 0; i < times ; i++)
-            {
-                s = $"{times.ToString()}";
-            }
-            sw.Stop();
-            Console.WriteLine($"times.ToString():{sw.ElapsedMilliseconds}");
-
-            Console.ReadKey();
+            BenchmarkRunner.Run<BenchmarkMeasure>(new Config());
+            Console.Read();
         }
+    }
+
+
+    [BenchmarkCategory("Framework")]
+    [MemoryDiagnoser]
+    [SimpleJob(launchCount: 1, warmupCount: 1, targetCount: 1, invocationCount: 1, baseline: false)]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    public class BenchmarkMeasure
+    {
+        int intVal = 1;
+        int intVal1 = 2;
+        int intVal2 = 2;
+        int intVal3 = 3;
+
+
+        string stringVal = "Hello World";
+        [Benchmark(Description = "Int_NoUseToString")]
+        public void Int_NoUseToString()
+        {
+            string result = $"{intVal} {intVal2} {intVal3}";
+        }
+
+        [Benchmark(Description = "Int_UseToString")]
+        public void Int_UseToString()
+        {
+            string result = $"{intVal.ToString()} {intVal2.ToString()} {intVal3.ToString()}";
+        }
+
+        [Benchmark(Description = "String_NoUseToString")]
+        public void String_NoUseToString()
+        {
+            string result = $"{stringVal}";
+        }
+
+        [Benchmark(Description = "String_UseToString")]
+        public void String_UseToString()
+        {
+            string result = $"{stringVal.ToString()}";
+        }
+
     }
 }
