5b62c58ff32dc3f123f715c8cee4a70f17adb9a3
damiel <dog830228@gmail.com>
2021-07-14 22:24:46 +0800

    add sample.

---
+++ src/Samples/CAS_Lock/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CAS_Lock
+{
+
+    class Program
+    {
+        static object _lock = new object();
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            int balanceValue = 10000000;
+            Member member = new Member() { Balance = balanceValue };
+            List<Task> tasks = new List<Task>();
+            sw.Start();
+            for (int i = 0; i < 1000000; i++)
+            {
+                tasks.Add(Task.Run(() => member.UpdateBalance()));
+            }
+            Task.WaitAll(tasks.ToArray());
+            sw.Stop();
+            Console.WriteLine("Lock Version");
+            Console.WriteLine($"member remaining balance is {member.Balance}");
+            Console.WriteLine($"Exec Time Cost : {sw.ElapsedMilliseconds}");
+
+            tasks.Clear();
+            member.Balance = balanceValue;
+            sw.Restart();
+            for (int i = 0; i < 1000000; i++)
+            {
+                tasks.Add(Task.Run(() => member.UpdateBalanceByInterlock()));
+            }
+            Task.WaitAll(tasks.ToArray());
+            sw.Stop();
+            Console.WriteLine("InterLocked Version:");
+            Console.WriteLine($"member remaining balance is {member.Balance}");
+            Console.WriteLine($"Exec Time Cost : {sw.ElapsedMilliseconds}");
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Member
+    {
+        object _lock = new object();
+
+        public int Balance { get; set; }
+
+        public void UpdateBalance()
+        {
+            lock (_lock)
+            {
+                Balance -= 10;
+            }
+        }
+
+        public void UpdateBalanceByInterlock()
+        {
+            int val = 0;
+            Balance = Interlocked.Exchange(ref val, Balance -= 10);
+        }
+    }
+}
---
+++ src/Samples/VolatileSample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace VolatileSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int val = Convert.ToInt32(Console.ReadLine());
+            Member member = new Member() { Balance = val };
+
+            var t1 = new Thread(member.UpdateBalance);
+            t1.Start();
+
+            while (true)
+            {
+                if (member.Balance <= 0)
+                {
+                    Console.WriteLine("餘額小於0");
+                    break;
+                }
+              
+            }
+            Console.WriteLine("執行結束!");
+            Console.ReadKey();
+        }
+    }
+
+    public class Member
+    {
+        //public volatile int Balance;
+        public int Balance;
+        public void UpdateBalance()
+        {
+            Console.WriteLine("開始扣款");
+            while (Balance > 0)
+            {
+                Balance -= 10;
+                //Thread.Sleep(200);
+            }
+            Console.WriteLine($"餘額={Balance}");
+        }
+    }
+}
