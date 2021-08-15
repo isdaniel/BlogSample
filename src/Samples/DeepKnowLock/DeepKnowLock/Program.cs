078e953730718bd43870a2f0a67f63febc64f0da
Daniel <dog830228@gmail.com>
2021-08-15 14:21:51 +0800

    add deep lock sample.

---
+++ src/Samples/DeepKnowLock/DeepKnowLock/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DeepKnowLock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object _object = new object();
+
+            Task.WaitAll(Task.Run(() => { TryLockDemo(_object); }), Task.Run(() => { TryLockDemo(_object); }));
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void TryLockDemo(object _object) {
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine($"[{threadId}] {DateTime.Now:HH:mm:ss} TryLockDemo Start");
+            try
+            {
+                Monitor.Enter(_object);
+                Console.WriteLine($"[{threadId}] {DateTime.Now:HH:mm:ss} get first lock");
+                try
+                {
+
+                    Monitor.Enter(_object);
+                    Console.WriteLine($"[{threadId}] {DateTime.Now:HH:mm:ss} get second lock");
+                }
+                finally
+                {
+                    Monitor.Exit(_object);
+                    Console.WriteLine($"[{threadId}] {DateTime.Now:HH:mm:ss} release second lock");
+                }
+            }
+            finally
+            {
+                Monitor.Exit(_object);
+                Console.WriteLine($"[{threadId}] {DateTime.Now:HH:mm:ss} release first lock");
+            }
+        }
+    }
+}
