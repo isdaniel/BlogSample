ebfb4038245744b21b2dec25617ef733204b883c
DanielShih <dog830228@gmail.com>
2021-11-04 17:37:34 +0800

    add cloud watch sample

---
+++ src/AWS_Sample/CloudWatch_sample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.AWS.Logger;
+using NLog.Config;
+using LogLevel = NLog.LogLevel;
+
+namespace CloudWatch_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            var config = new LoggingConfiguration();
+
+            var awsTarget = new AWSTarget()
+            {
+                LogGroup = ".netcore-group",
+                Region = "ap-northeast-1",
+                Layout = "${message}",
+                LogStreamNameSuffix = "Daniel-logstream"
+            };
+            config.AddTarget("aws", awsTarget);
+
+            config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, awsTarget));
+
+            LogManager.Configuration = config;
+            
+            //write your log
+            var logger = LogManager.GetCurrentClassLogger();
+            logger.Info("Hello World by Daniel");
+        }
+    }
+}
