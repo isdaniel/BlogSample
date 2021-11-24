6da237be22f8e7b531c7ee5ef03bc350e0bebe7b
DanielShih <dog830228@gmail.com>
2021-11-24 14:10:41 +0800

    modify

--- src/Algorithm_DataStructure/Maze/Program.cs
+++ src/Algorithm_DataStructure/Maze/Program.cs
@@ -4,35 +4,30 @@ namespace Maze
 {
     class Program
     {
+        //策略 下 => 右 => 左 => 上
         static bool SearchMap(int[,] map,int x,int y){
-            //終點
-            if (map[6,6] == 2)
+            if(map[6,6] == 2)
                 return true;
-            
-            if(map[x,y] == 0){
-                map[x,y] = 2;
-                if (SearchMap(map,x + 1,y))
-                {
+
+            if (map[y,x] == 0)
+            {
+                map[y,x] = 2;
+                if (SearchMap(map,x,y + 1))
                     return true;
-                }//右
-                else if (SearchMap(map,x,y+ 1)){
+                else if (SearchMap(map,x + 1,y))
                     return true;
-                }//左
-                else if (SearchMap(map,x,y - 1)){
+                else if (SearchMap(map,x - 1,y))
                     return true;
-                }//上
-                else if (SearchMap(map,x- 1,y)){
+                else if (SearchMap(map,x ,y - 1))
                     return true;
-                }else{
-                    map[x,y] = 3;
-                    return false;
+                else{
+                    map[y,x] = 3;
                 }
             }
-            
+
             return false;
-            
         }
-        //策略 下 => 右 => 左 => 上
+        
         static void Main(string[] args)
         {
             /*
@@ -41,6 +36,18 @@ namespace Maze
             3 代表死路
             */
             int[,] map = new int[8, 8];
+            CreateWall(map);
+            
+            
+            System.Console.WriteLine(SearchMap(map, 1, 1)); ;
+            System.Console.WriteLine("===============================");
+            PrintMap(map);
+            //start at 1,1
+
+        }
+
+        private static void CreateWall(int[,] map)
+        {
             for (int i = 0; i < 8; i++)
             {
                 map[0, i] = 1;
@@ -48,19 +55,13 @@ namespace Maze
                 map[i, 0] = 1;
                 map[i, 7] = 1;
             }
-
-            map[2,1] = 1;
-            map[2,2] = 1;
-            map[2,3] = 1;
-            map[2,4] = 1;
-            map[2,5] = 1;
-            map[2,6] = 1;
-            PrintMap(map);
-            System.Console.WriteLine(SearchMap(map,1,1));;
-            System.Console.WriteLine("===============================");
-            PrintMap(map);
-            //start at 1,1
-
+            
+            map[2, 1] = 1;
+            map[2, 2] = 1;
+            map[2, 3] = 1;
+            map[2, 4] = 1;
+            map[2, 5] = 1;
+            //map[2, 6] = 1;
         }
 
         private static void PrintMap(int[,] map)
