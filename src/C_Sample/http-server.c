3c116c8dbfd9ae07b5d455e2346dbdeee3098cbc
danielshih <danielshih@microsoft.com>
2023-01-02 22:04:29 +0800

    add spin lock sample

--- src/C_Sample/http-server.c
+++ src/C_Sample/http-server.c
@@ -2,18 +2,20 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netinet/ip.h> 
+#include <netinet/ip.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int server_fd, client_fd;
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_addr_len;
 
     // 建立網路套接字
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
         perror("socket error");
         exit(1);
     }
@@ -25,37 +27,42 @@ int main(int argc, char *argv[]) {
     server_addr.sin_port = htons(8000);
 
     // 綁定地址和端口
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
         perror("bind error");
         exit(1);
     }
 
     // 設置監聽
-    if (listen(server_fd, 5) < 0) {
+    if (listen(server_fd, 5) < 0)
+    {
         perror("listen error");
         exit(1);
     }
 
-    while (1) {
-      // 接受客戶端連接
-      client_addr_len = sizeof(client_addr);
-      if ((client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len)) < 0) {
-          perror("accept error");
-          continue;
-      }
+    while (1)
+    {
+        // 接受客戶端連接
+        client_addr_len = sizeof(client_addr);
+        if ((client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len)) < 0)
+        {
+            perror("accept error");
+            continue;
+        }
 
-      // 接收客戶端請求並回傳回應
-      char request[1024];
-      int recv_len = recv(client_fd, request, sizeof(request), 0);
-      if (recv_len < 0) {
-          perror("recv error");
-          close(client_fd);
-          continue;
-      }
+        // 接收客戶端請求並回傳回應
+        char request[1024];
+        int recv_len = recv(client_fd, request, sizeof(request), 0);
+        if (recv_len < 0)
+        {
+            perror("recv error");
+            close(client_fd);
+            continue;
+        }
 
-      // 解析 HTTP 請求並發送回應
-      char *response = "HTTP/1.1 200 OK\nContent-Type: text/html\n\n<html><body><h1>Hello, World!</h1></body></html>\n";
-      send(client_fd, response, strlen(response), 0);
-      close(client_fd);
-  }
+        // 解析 HTTP 請求並發送回應
+        char *response = "HTTP/1.1 200 OK\nContent-Type: text/html\n\n<html><body><h1>Hello, World!</h1></body></html>\n";
+        send(client_fd, response, strlen(response), 0);
+        close(client_fd);
+    }
 }
---
+++ src/C_Sample/spin-lock.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int lock = 0;
+
+void *thread_func(void* arg)
+{
+    // Try to acquire the lock
+    while (__sync_lock_test_and_set(&lock, 1));
+
+    // Critical section
+    printf("Inside critical section %d \n",*((int*)arg));
+
+    // Release the lock
+    __sync_lock_release(&lock);
+}
+
+int main(void)
+{
+    printf("spin lock start\r\n");
+    pthread_t thread1, thread2;
+    int* ptr1= malloc(sizeof(int)); 
+    int* ptr2= malloc(sizeof(int));
+    *ptr1 = 1;
+    *ptr2 = 2;
+    pthread_create(&thread1, NULL, thread_func, ptr1);
+    pthread_create(&thread2, NULL, thread_func, ptr2);
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+    return 0;
+}
