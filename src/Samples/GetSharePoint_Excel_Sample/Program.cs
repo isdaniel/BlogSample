573b579362daf193bb788bc5fbdfdcdf7cc3d8d5
danielshih <dog830228@gmail.com>
2023-04-11 17:24:18 +0800

    add office 365 read excel sample.

---
+++ src/Samples/GetSharePoint_Excel_Sample/Program.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Graph;
+using Microsoft.Identity.Client;
+using Microsoft.Identity.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace Excel365Test
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                RunAsync().GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+
+        private static async Task RunAsync()
+        {
+            AuthenticationConfig config = AuthenticationConfig.ReadFromJsonFile("appsettings.json");
+            // Even if this is a console application here, a daemon application is a confidential client application
+                        
+            IConfidentialClientApplication app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                    .WithClientSecret(config.ClientSecret)
+                    //.WithAuthority(new Uri(config.Authority))
+                    .Build();
+
+            app.AddInMemoryTokenCache();
+
+            // With client credentials flows the scopes is ALWAYS of the shape "resource/.default", as the 
+            // application permissions need to be set statically (in the portal or by PowerShell), and then granted by
+            // a tenant administrator. 
+            string[] scopes = new string[] { $"{config.ApiUrl}.default"  }; // Generates a scope -> "https://graph.microsoft.com/.default"
+
+            // Call MS graph using the Graph SDK
+            await CallMSGraphUsingGraphSDK(app, scopes);
+        }
+
+        /// <summary>
+        /// The following example shows how to initialize the MS Graph SDK
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="scopes"></param>
+        /// <returns></returns>
+        private static async Task CallMSGraphUsingGraphSDK(IConfidentialClientApplication app, string[] scopes)
+        {
+            // Prepare an authenticated MS Graph SDK client
+            GraphServiceClient graphServiceClient = GetAuthenticatedGraphClient(app, scopes);
+
+            try
+            {
+            var workbook = await graphServiceClient.Users["7b86cadd-9bb8-4e0e-ab93-e893d29e62db"].Drive
+                    .Items["01OEBGDDPV7HQXMOOY45H3AZE2L3VJES23"].Workbook
+                    .Worksheets["Details"].Range("H1:I69").Request().GetAsync();
+
+                foreach (var rows in workbook.Text.Deserialize<List<List<string>>>())
+                {
+                    System.Console.WriteLine(string.Join(",", rows.Select(x=> string.IsNullOrEmpty(x) ? "N/A": x)));
+                }
+                await Console.Out.WriteLineAsync();
+            }
+            catch (ServiceException e)
+            {
+                Console.WriteLine("We could not retrieve the user's list: " + $"{e}");
+            }
+
+        }
+
+        /// <summary>
+        /// An example of how to authenticate the Microsoft Graph SDK using the MSAL library
+        /// </summary>
+        /// <returns></returns>
+        private static GraphServiceClient GetAuthenticatedGraphClient(IConfidentialClientApplication app, string[] scopes)
+        {
+
+            GraphServiceClient graphServiceClient =
+                    new GraphServiceClient("https://graph.microsoft.com/V1.0/", new DelegateAuthenticationProvider(async (requestMessage) =>
+                    {
+                        // Retrieve an access token for Microsoft Graph (gets a fresh token if needed).
+                        AuthenticationResult result = await app.AcquireTokenForClient(scopes)
+                            .ExecuteAsync();
+
+                        // Add the access token in the Authorization header of the API request.
+                        requestMessage.Headers.Authorization =
+                            new AuthenticationHeaderValue("Bearer", result.AccessToken);
+                    }));
+            return graphServiceClient;
+        }
+    }
+}
