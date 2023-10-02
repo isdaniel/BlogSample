5785481c4606bf72c45f87f612001b6674a54197
danielshih <dog830228@gmail.com>
2023-10-02 17:51:02 +0800

    add KMP sample

---
+++ src/Algorithm_DataStructure/KMP_sample/Program.cs
@@ -0,0 +1,46 @@
+﻿
+System.Console.WriteLine(KMP("abababcaa","ababcc"));
+
+bool KMP(string source,string subStr){
+    int i = 0;
+    int j = 0;
+    int alen = source.Length;
+    int blen = subStr.Length;
+    var next = Build_NextTable(subStr);
+    while (i < alen && j < blen)
+    {
+        if(source[i] == subStr[j]){
+            i++;
+            j++;
+        } else if (j > 0){
+            j = next[j - 1];
+        } else {
+            i++;
+        }
+
+        if(j == blen){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+//PMT
+int[] Build_NextTable(string s){
+    int[] res = new int[s.Length];
+    int prefix_len = 0;
+    for(int i = 1; i < s.Length; i++)
+    {
+        if(s[prefix_len] == s[i]){
+            prefix_len++;
+            res[i] = prefix_len;
+        } else {
+            prefix_len = prefix_len - 1 < 0 ? 0 : res[prefix_len - 1];
+            if(prefix_len == 0){
+                res[i] = 0;
+            }
+        }
+    }
+    return res;
+}
---
+++ src/Algorithm_DataStructure/MonotonicStack/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+using System.Data;
+using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    private static void Main(string[] args){
+        foreach (var n in FindRightSamll(new int[]{1,2,4,9,4,0,5}))
+        {
+            System.Console.WriteLine(n);
+        }
+    }
+
+    static int[] FindRightSamll(int[] arr){
+        int[] res = new int[arr.Length];
+        Stack<int> stack = new Stack<int>();
+
+        for(int i = 0; i < arr.Length; i++)
+        {
+            while (stack.Count > 0 && arr[i] < arr[stack.Peek()])
+            {
+                res[stack.Pop()] = arr[i];
+            }
+
+            stack.Push(i);
+        }
+
+        while (stack.Count > 0)
+        {
+            res[stack.Pop()] = -1;
+        }
+
+        return res;
+    }
+}
+
