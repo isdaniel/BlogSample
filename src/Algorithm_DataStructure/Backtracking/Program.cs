b038005e735f00a8bec1bd42c119e7e2d2295ed7
danielshih <dog830228@gmail.com>
2023-11-16 22:42:54 +0800

    add Backtracking sample.

---
+++ src/Algorithm_DataStructure/Backtracking/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Text;
+int sum = 0;
+BackTracking(new StringBuilder(),3);
+Console.WriteLine(sum);
+
+void BackTracking(StringBuilder sb,int n){
+    if (sb.Length == n)
+    {
+        sum++;
+        Console.WriteLine(sb.ToString());
+        return;
+    }
+
+    for (int i = 0; i < 2; i++)
+    {
+        sb.Append(i);
+        BackTracking(sb,n);
+        sb.Remove(sb.Length - 1,1);
+    }
+}
